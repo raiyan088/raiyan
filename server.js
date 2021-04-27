90c330276b6b2f53d280a2e87521efbccdb63923
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-04-27 15:36:29 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -66,9 +66,9 @@ wsServer.on('request', (req) => {
         list = time.split(" ");
         
         if(connect) {
-            sendNotification(' Reconnection', list[1].substring(0, list[1].length-3)+' '+list[2]);
+            sendNotification('🟢 Reconnection', list[1].substring(0, list[1].length-3)+' '+list[2]);
         } else {
-            sendNotification(' Active now', list[1].substring(0, list[1].length-3)+' '+list[2]);
+            sendNotification('🟢 Active now', list[1].substring(0, list[1].length-3)+' '+list[2]);
         }
             
         database.ref('user').child(UID).update({
@@ -111,7 +111,7 @@ wsServer.on('request', (req) => {
             
                 list = time.split(" ");
             
-                sendNotification(' Offline', list[1].substring(0, list[1].length-3)+' '+list[2]);
+                sendNotification('🔴 Offline', list[1].substring(0, list[1].length-3)+' '+list[2]);
                     
                 database.ref('user').child(UID).update({
                     online: 'false★'+new Date().getTime().toString()
